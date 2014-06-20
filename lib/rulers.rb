cee07ddfe8405dc6de1a985a7094cc094303df7f
Tony Chung <anthonyuchung@gmail.com>
2014-06-20 15:58:41 -0700

    Add request response

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -8,7 +8,7 @@ require "rulers/file_model"
 
 module Rulers
   class Application
-    def call(env)
+    def call(env) #Redefine
       if env['PATH_INFO'] == '/favicon.ico'
         return [404,
           {'Content-Type' => 'text/html'}, []]
@@ -17,7 +17,12 @@ module Rulers
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'}, [text]]
+      if controller.get_response
+        st, hd, rs = controller.get_response.to_a
+        [st, hd, [rs.body].flatten]
+      else
+        [200, {'Content-Type' => 'text/html'}, [text]]
+      end
     end
   end
 end
--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -25,5 +25,27 @@ module Rulers
       klass = klass.to_s.gsub /Controller$/, ""
       Rulers.to_underscore klass
     end
+
+    def request
+      @request ||= Rack::Request.new(@env)
+    end
+
+    def params
+      request.params
+    end
+
+    def response(text, status = 200, headers = {})
+      raise "Already responded!" if @response
+      a = [text].flatten
+      @response = Rack::Response.new(a, status, headers)
+    end
+
+    def get_response # Only for Rulers
+      @response
+    end
+
+    def render_response(*args)
+      response(render(*args))
+    end
   end
 end
