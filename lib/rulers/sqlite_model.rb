9dc898f48aec6f0e1c348b51bbe18933d5398f23
Tony Chung <anthonyuchung@gmail.com>
2014-06-20 17:05:30 -0700

    Add saving objects method

--- lib/rulers/sqlite_model.rb
+++ lib/rulers/sqlite_model.rb
@@ -73,6 +73,28 @@ SQL
       def []=(name, value)
         @hash[name.to_s] = value
       end
+
+      def save!
+        unless @hash["id"]
+          self.class.create
+          return true
+        end
+
+        fields = @hash.map do |k, v|
+          "#{k}=#{self.class.to_sql(v)}"
+        end.join ","
+
+        DB.execute <<SQL
+UPDATE #{self.class.table}
+SET #{fields}
+WHERE id = #{@hash["id"]}
+SQL
+        true
+      end
+
+      def save
+        self.save! rescue false
+      end
     end
   end
 end
